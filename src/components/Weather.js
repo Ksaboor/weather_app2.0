d65cdf4bc78de67e3b76539269000de6f7422276
Khail Saboor <khalilsaboor1@gmail.com>
2018-09-04 16:16:54 -0400

    React Conditional
    TRANSLATION if "this.props.temperature exist? then render out the <P> tags"

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -3,7 +3,17 @@ import React from 'react';
 class Weather extends React.Component{
     render(){
         return(
-            <div>Weather Component</div>
+            <div>
+                {/* here we have created a conditional operator so
+                 are p tags are shown only when the button
+                 is press using */}
+                {this.props.city && this.props.country && <p> Location: {this.props.city}, {this.props.country}</p>}
+                {/* TRANSLATION if "this.props.temperature exist? then render out the <P> tags"*/}
+                {this.props.temperature && <p>Temperature: {this.props.temperature}</p>}
+                {this.props.temperature && <p>Temperature: {this.props.temperature}</p>}
+                {this.props.temperature && <p>Humidity: {this.props.humidity}</p>}
+                {this.props.description && <p>Conditions: {this.props.description}</p>}
+            </div>
         );
     }
 };
