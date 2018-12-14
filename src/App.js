7b901ac298161c5776de994d588c43c762d5b91c
Khail Saboor <khalilsaboor1@gmail.com>
2018-12-14 11:54:09 -0500

    http

--- src/App.js
+++ src/App.js
@@ -40,7 +40,7 @@ class App extends React.Component{
         e.preventDefault();
         const city = e.target.elements.city.value;
         const country = e.target.elements.country.value;
-        const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&mode=json&appid=${API_KEY}&units=imperial`);
+        const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&mode=json&appid=${API_KEY}&units=imperial`);
         const data = await api_call.json();
         //added if statements to avoid runtime error
         //only when both city and country are equal to true
@@ -74,18 +74,33 @@ class App extends React.Component{
         render(){
             return (
                 <div>
-                    <Titles />
-                    <Form getWeather={this.getWeather}/>
-                    <Weather
-                        temperature={this.state.temperature}
-                        city={this.state.city}
-                        country={this.state.country}
-                        humidity={this.state.humidity}
-                        description={this.state.description}
-                        error={this.state.error}
-                    />
+                <div className="wrapper">
+                    <div className="main">
+                        <div className="container">
+                            <div className="row">
+                                <div className="col-xs-5 title container">
+                                    <Titles />
+                                </div>
+                                <div className="col-xs-7 form-container">
+                                        <Form getWeather={this.getWeather}/>
+                                            <Weather
+                                                temperature={this.state.temperature}
+                                                city={this.state.city}
+                                                country={this.state.country}
+                                                humidity={this.state.humidity}
+                                                description={this.state.description}
+                                                error={this.state.error}
+                                            />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
                 </div>
             );
         }
 }
+
+
+
 export default App;
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -3,17 +3,42 @@ import React from 'react';
 class Weather extends React.Component{
     render(){
         return(
-            <div>
+            <div className="weather__info">
                 {/* here we have created a conditional operator so
                  are p tags are shown only when the button
                  is press using */}
-                {this.props.city && this.props.country && <p> Location: {this.props.city}, {this.props.country}</p>}
+                
+                {
+                    this.props.city && this.props.country && <p className="weather__key"> Location: 
+                    <span className="weather__value"> {this.props.city}, {this.props.country}</span>
+                    </p>
+                }
                 {/* TRANSLATION if "this.props.temperature exist? then render out the <P> tags"*/}
-                {this.props.temperature && <p>Temperature: {this.props.temperature}</p>}
-                {this.props.temperature && <p>Temperature: {this.props.temperature}</p>}
-                {this.props.temperature && <p>Humidity: {this.props.humidity}</p>}
-                {this.props.description && <p>Conditions: {this.props.description}</p>}
-                {this.props.error && <p>{this.props.error}</p>}
+                {
+                    this.props.temperature && <p className="weather__key">Temperature: 
+                    <span className="weather__value"> {this.props.temperature}</span>
+                    </p>
+                }
+                {
+                    this.props.temperature && <p className="weather__key">Temperature: 
+                    <span className="weather__value"> {this.props.temperature}</span>
+                    </p>
+                }
+                {
+                    this.props.temperature && <p className="weather__key">Humidity: 
+                    <span className="weather__value"> {this.props.humidity}</span>
+                    </p>
+                }
+                {
+                    this.props.description && <p className="weather__key">Conditions: 
+                    <span className="weather__value"> {this.props.description}</span>
+                    </p>
+                }
+                {
+                    this.props.error && <p className="weather__error"> 
+                    {this.props.error}
+                    </p>
+                }
             </div>
         );
     }
